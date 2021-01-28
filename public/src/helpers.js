175e4c76ec72e063da7ff20641e3b9d69c5f14b0
austinhjones3 <austin@austinjones.io>
2021-01-28 15:37:35 -0600

    more formatting

--- public/src/helpers.js
+++ public/src/helpers.js
@@ -1,24 +1,19 @@
-
 // booksOut and booksIn tests the first element of the books' borrows array
 // they return an array of the elements that match
-const booksOut = books => {
-    return books.filter(book => book.borrows[0].returned === false);
-}
+const booksOut = (books) => {
+  return books.filter((book) => book.borrows[0].returned === false);
+};
 
-
-const booksIn = books => {
-    return books.filter(book => book.borrows[0].returned === true);
-}
-
+const booksIn = (books) => {
+  return books.filter((book) => book.borrows[0].returned === true);
+};
 
 const booksOutForAccount = (booksOutArr, account) => {
-    return booksOutArr.filter(element => element.borrows[0].id === account.id);
-}
-
-
+  return booksOutArr.filter((element) => element.borrows[0].id === account.id);
+};
 
 module.exports = {
-    booksOut,
-    booksIn,
-    booksOutForAccount,
-}
+  booksOut,
+  booksIn,
+  booksOutForAccount,
+};

4b84abe71974db54a19108c2291eec66b5b04e88
Austin Jones <austin@austinjones.io>
2021-02-01 14:58:14 -0600

    src/accounts.js appropriate comments added and minor refactors

--- public/src/README.md
+++ public/src/README.md
@@ -2,14 +2,5 @@
 
 ### Important notes about design decisions
 
-- #### .slice()
-Takes two arguments: the index to start from, and the amount of elements to leave in the array.
- 
-- #### using sets to get uniques
-I found a handy little data type called 'sets' and I used its properties to make unique arrays from dupe arrays. I'm not sure if there was another way to do this that was possibly easier, but at least I learned something!
-
-- #### node require error
-In books.js on line 36, I spread a function `...account => account.id === element.id` that returns an object. There is another function `findAccountById()` in accounts.js that I could have imported and used to get this object. However, this resulted in errors because books and accounts required each other. Node did not like require statements pointing at each other so I had to drop one of them.
-
 - #### using .forEach() instead of .reduce() in some cases
-I found that `.forEach()` helped me write some functions a little cleaner than `.reduce()` would have, so in some cases `.reduce()` is not used where it could be. If I am wrong, please let me know!
+  I found that `.forEach()` helped me write some functions a little cleaner than `.reduce()` would have, so in some cases `.reduce()` is not used where it could be. If I am wrong, please let me know!
--- public/src/accounts.js
+++ public/src/accounts.js
@@ -5,12 +5,11 @@ function findAccountById(accounts, testId) {
 }
 
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((accountA, accountB) => {
-    return accountA.name.last > accountB.name.last ? 1 : -1;
-  });
+  return accounts.sort((a, b) => (a.name.last > b.name.last ? 1 : -1));
 }
 
 function numberOfBorrows({ id: accountId }, books) {
+  // accumulate and return the given account's number of borrows
   return books.reduce((total, book) => {
     const borrowIdArr = book.borrows.map((element) => element.id);
     if (borrowIdArr.includes(accountId)) total++;
@@ -19,15 +18,20 @@ function numberOfBorrows({ id: accountId }, books) {
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  return books
-    .filter((book) => {
-      const latestBorrow = book.borrows[0];
-      return !latestBorrow.returned && latestBorrow.id === account.id;
-    })
-    .map((book) => {
-      const author = findAuthorById(authors, book.authorId);
-      return { ...book, author };
-    });
+  // return an array of all books and their authors possessed by the account
+  return (
+    books
+      // filter for only the books that are checked out by the account
+      .filter((book) => {
+        const latestBorrow = book.borrows[0];
+        return !latestBorrow.returned && latestBorrow.id === account.id;
+      })
+      // map out data for those books: book keys and associated author object
+      .map((book) => {
+        const author = findAuthorById(authors, book.authorId);
+        return { ...book, author };
+      })
+  );
 }
 
 module.exports = {
